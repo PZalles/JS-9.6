a9a44f886fcedf3dfb77c0e1fedf25c9a41e11f9
Przemyslaw Zalewski <przemyslawwys@wp.pl>
2018-02-08 20:27:33 +0100

    Add js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
-var withButtonItemClass = document.getElementsByClassName('button');
+var list = document.getElementById('list');
 
-    for (var i = 0; i < withButtonItemClass.length; i++) {
-        var text = withButtonItemClass[i].innerText;
-        console.log(text);
-    };
+var add = document.getElementById('addElem');
+
+add.addEventListener('click', function() {
+  var element = document.createElement('li');
+  var x = document.getElementsByTagName('li').length;
+  element.innerHTML = 'item ' + x ;
+  
+  list.appendChild(element);
+});
